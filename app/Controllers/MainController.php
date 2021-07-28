751f0439966af54a2f2cfa1b8442dfd2cf20aaec
Christophe Deneuve <33314621+christopheOclock@users.noreply.github.com>
2021-07-28 13:11:02 +0200

    Initial commit

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,61 @@
+<?php
+/************* MainController ******************* 
+  
+Je suis le seul controller (pour l'instant)
+
+Mon rôle est de donner les informations nécéssaires
+à l'affichage d'une vue.
+
+C'est aussi moi qui fait les require
+je suis donc en charge de la méthode show()
+
+*******************************************/
+
+class MainController {
+
+    public function affichePageHome(){
+        // j'utilise la méthode show avec les bons paramètres
+        // je sais que cette méthode doit affiche la page store, obvious, le nom de la méthode
+        $this->show('home', "Je suis la page HOME");
+    }
+
+    /**
+     * Fonction qui require les templates HEADER / FOOTER
+     * Ainsi que la vue donnée en paramètre
+     * Et surtout les informations nécessaire/essentielle à l'affichage vue
+     *
+     * @param string $viewName le nom du fichier template à inclure
+     * @param $viewData Contient toutes les informations que j'ai besoin pour l'affichage
+     */
+    /* avant de rentrer dans la fonction show
+    $viewName = $currentPage;
+    $viewData = $weekOpeningHours;
+    */
+    public function show($viewName, $viewData)
+    {
+        require_once __DIR__.'/../views/header.tpl.php';
+
+        /****************************************** */
+        // exemples : require_once __DIR__."/views/{$viewName}.tpl.php";
+        // si $viewName == 'home'   
+        // require_once __DIR__."/views/home.tpl.php";
+        // si $viewName == 'products'   
+        // require_once __DIR__."/views/products.tpl.php";
+        // si $viewName == 'store'   
+        // require_once __DIR__."/views/store.tpl.php";
+        /****************************************** */
+        
+        // ici je peux utiliser $viewData
+        // donc dans le require aussi
+
+        require_once __DIR__."/../views/{$viewName}.tpl.php";
+        require_once __DIR__.'/../views/footer.tpl.php';
+
+        /* A la manière de la Saison 4 */
+        //require __DIR__.'/views/header.tpl.php';
+        // require __DIR__."/views/{$currentPage}.tpl.php";
+        // Autre manière de concatener notre variable
+        // require __DIR__.'/views/'.$currentPage.'.tpl.php';
+        //require __DIR__.'/views/footer.tpl.php';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,70 @@
+<?php
+/************* Point d'entrée ******************* 
+  
+Je suis le point d'entrée
+tout le monde passe par moi pour voir le site.
+
+Je m'occupe de récupérer les informations
+$_GET, $_POST
+
+Avec ces informations, je choisit quelle vue/template doit être afficher
+
+C'est moi qui dit qu'elle est la page par défaut
+puisque c'est moi qui choisit la vue à afficher
+
+Pas d'info => page par défaut
+(ça rime donc c'est vrai :D)
+
+*******************************************/
+
+/**********************************
+//?    Require des fichiers de classe / functions / BDD / etc ...
+**********************************/
+
+// j'ai besoin du fichier pour la function show()
+// require __DIR__.'/functions.php';
+require __DIR__.'/../app/Controllers/MainController.php';
+
+
+/**********************************
+//?    Je m'occupe de $_GET et je définis la vue à afficher
+//?    Par défaut la vue sera 'home'
+**********************************/
+
+// exemple de lien : index.php?page=store
+
+// on vérifie que l'utilisateur nous donne bien l'information
+// de la page qu'il veux, sinon on lui donne la page par défaut
+if (isset($_GET['page'])) {
+    // on récupère la page demandé par l'utilisateur
+    $currentPage = $_GET['page'];
+} else {
+    // notre page par défaut
+    $currentPage = "home";
+}
+
+//? ici nous avons la même condition mais sur une seule ligne :
+//?  expression ternaire (ternaire : 3)
+/*
+ est ce que isset($_GET['page'])  est VRAI ?
+ après le ? le résultat si VRAI
+ après le : le résultat si FAUX
+*/
+// $currentPage = isset($_GET['page']) ? $_GET['page'] : 'home'
+
+
+/**********************************
+//?    Je dois fournir des informations à mes vues
+//?    Ici je donne les informations suivant la vue que l'on a demandé
+**********************************/
+// Je créer une instance de MainController
+// C'est lui qui s'occupe de la méthode show()
+$controller = new MainController();
+
+if ($currentPage == 'home') {
+    // comme c'est le rôle du controller
+    // je lui demande d'afficher la page home
+    // sans rien savoir de ce que le controller va faire
+    $controller->affichePageHome();
+}
+
